a37d82fe31440b5bf8955429dda5c961973a3f83
RafaelAHoffmann <148063811+RafaelAHoffmann@users.noreply.github.com>
2024-03-13 09:31:50 -0300

    13032024-5 main.js hotfix

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-const botoes = document.querySelectorAll("botao");
+const botoes = document.querySelectorAll(".botao");
 
 for (let i = 0; i < botoes.length; i++)
 {
-    botoes.onclick = function(){
+    botoes[i].onclick = function(){
+
         for (let j = 0; j < botoes.length; j++){
             botoes.classList.remove("ativo");
         }
+
         botoes[i].classList.add("ativo");
     }
 }
